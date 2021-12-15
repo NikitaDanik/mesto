cb778843055aaeee5fc4004b8cb2754fd41d6c63
Nikita Danik <samsacc17@mail.ru>
2021-12-16 01:05:46 +0300

    create show/hide inputErrors

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,3 +5,18 @@ const checkInputValidity = (formElement, inputElement, errorClass, inputErrorCla
         showInputError(formElement, inputElement.validationMessage, errorClass,inputErrorClass);;
     }
 }
+
+const showInputError = (formElement, inputElement, errorMessage, errorClass, inputErrorClass) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id} = error`);
+    inputElement.classList.add(inputErrorClass);
+    errorElement.classList.add(errorClass);
+    errorElement.textContent = errorMessage;
+}
+
+const hideInputError = (formElement, inputElement, errorClass, inputErrorClass) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id} = error`);
+    inputElement.classList.remove(inputErrorClass);
+    errorElement.classList.remove(errorClass);
+    errorElement.textContent = '';
+}
+
